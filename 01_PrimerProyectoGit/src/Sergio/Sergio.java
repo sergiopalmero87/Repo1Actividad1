4b6778470275a9956c5a90d1a8bbe9b33ea80d48
sergio palmero <sergiopalmero87@gmail.com>
2023-02-02 14:46:05 +0100

    Creado metodo hobbie

--- 01_PrimerProyectoGit/src/Sergio/Sergio.java
+++ 01_PrimerProyectoGit/src/Sergio/Sergio.java
@@ -4,5 +4,9 @@ public class Sergio {
 	public void saludo() {
 		System.out.println("Hola, soy Sergio");
 	}
+	
+	public void hobbie() {
+		System.out.println("Mis hobbies son el deporte, la musica y la comida");
+	}
 
 }

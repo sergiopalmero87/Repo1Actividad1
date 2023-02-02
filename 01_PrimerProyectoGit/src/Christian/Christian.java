9aaf432a194e974eda50f2ad7e09f12278eb8f80
christian.casado <christian.casado@gmail.com>
2023-02-02 21:10:05 +0700

    arreglado el metodo de la clase Christian

--- 01_PrimerProyectoGit/src/Christian/Christian.java
+++ 01_PrimerProyectoGit/src/Christian/Christian.java
@@ -3,7 +3,9 @@ package Christian;
 public class Christian {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hola, soy Christian");
+		
 		System.out.println("Mis hobbies son viajar, leer y ver series");
 
 	}
